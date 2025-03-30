c95c71a55dd090ef4f0ce8a2fa876f1ff54c233d
Niranjana J <126334892+Niranjana372004@users.noreply.github.com>
2025-03-30 17:06:46 +0530

    finance_management_db.sql

--- finance_db.sql
+++ finance_db.sql
@@ -31,3 +31,33 @@ CREATE TABLE Expenses (
     CONSTRAINT Expenses_categoryid_fk FOREIGN KEY (category_id) 
           REFERENCES ExpenseCategories(category_id)
 );
+INSERT INTO Users (username, password, email) VALUES
+('john_doe', 'password123', 'john.doe@example.com'),
+('jane_smith', 'securepass456', 'jane.smith@example.com'),
+('mike_jones', 'mikepass789', 'mike.jones@example.com'),
+('emily_davis', 'emilypass321', 'emily.davis@example.com'),
+('chris_wilson', 'chrispass654', 'chris.wilson@example.com'),
+('laura_brown', 'laurapass987', 'laura.brown@example.com'),
+('kevin_miller', 'kevinpass111', 'kevin.miller@example.com');
+Select * from users;
+
+INSERT INTO ExpenseCategories (category_name) VALUES
+('Food'),
+('Transportation'),
+('Utilities'),
+('Entertainment'),
+('Healthcare'),
+('Education'),
+('Shopping');
+select * from ExpenseCategories;
+
+INSERT INTO Expenses (user_id, category_id, amount, date, description) VALUES
+(1, 1, 50.00, '2024-03-01', 'Grocery shopping'),
+(2, 2, 20.00, '2024-03-02', 'Bus fare'),
+(3, 3, 100.00, '2024-03-03', 'Electricity bill'),
+(4, 4, 30.00, '2024-03-04', 'Movie tickets'),
+(5, 5, 200.00, '2024-03-05', 'Doctor consultation'),
+(6, 6, 500.00, '2024-03-06', 'Course fee'),
+(7, 7, 150.00, '2024-03-07', 'Clothing shopping');
+Select * from Expenses;
+
