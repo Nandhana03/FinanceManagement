e27a8f8f11ef71d29fb6d92e9454d7009ccbdb6b
Katta Rohith <126334892+Niranjana372004@users.noreply.github.com>
2025-04-19 22:07:28 +0530

    ConnectionTest on users

---
+++ FinanceManagement/src/com/hexaware/fms/exception/ExpenseNotFoundException.java
@@ -0,0 +1,7 @@
+package com.hexaware.fms.exception;
+
+public class ExpenseNotFoundException extends Exception {
+    public ExpenseNotFoundException(String message) {
+        super(message);
+    }
+}
